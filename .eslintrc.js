adb5c7e5d7d31850ec996d5d7d8487beb67887b8
imsobusy <asgard774@hanyang.ac.kr>
2021-07-31 19:06:06 +0900

    Add lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,24 @@ module.exports = {
     es2021: true,
     es6: true,
     node: true,
+    jest: true,
   },
-  extends: [
-    'plugin:eslint/recommended',
-    'airbnb',
-    'plugin:prettier/recommended',
-  ],
+  extends: ["airbnb", "plugin:prettier/recommended"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: ['react'],
+  plugins: ["react"],
   rules: {
-    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
   },
 };
