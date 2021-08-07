37dd1ef0ea868fc4ee05343e3599fe65b0469658
hanyangaiis@subwaymate.cafe24app.com:hanyangaiis_subwaymate <rlawlsgh1111@naver.com>
2021-08-07 16:24:16 +0900

    test #8

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacttt
+          Learn Reactt
         </a>
       </header>
     </div>
