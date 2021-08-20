e9585d14a24bd71470fa9726a17ee91bffd199bf
hanyangaiis@subwaymate.cafe24app.com:hanyangaiis_subwaymate <rlawlsgh1111@naver.com>
2021-08-20 12:57:11 +0900

    Make Page4s

---
+++ src/component/Page4/component/page4s.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Page/Page4_css/page4s.css";
+
+function page4s() {
+  return (
+    <>
+      <div className="container">
+        <div>Hello World</div>
+      </div>
+    </>
+  );
+}
+
+export default page4s;
