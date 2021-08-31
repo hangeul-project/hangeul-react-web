d86bdc343ba9e2b0bb459d41942d8233204bb67b
zinozzing <rlawlsgh1111@naver.com>
2021-08-31 13:35:05 +0900

    git stash

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from "react";
 // import "./App.css";
 import Page4s from "./component/Page4/component/page4s";
 // import Page4 from "./component/Page4/component/page4";
+<<<<<<< Updated upstream
 // import Page1 from "./component/Page1/Page1";
 import QuizContainer from "./component/Page3/UI/QuizContainer";
 
 function App() {
   // TODO roll back to Page4s
   return <QuizContainer />;
+=======
+import Page1 from "./component/Page1/Page1"
+import QuizContainer from "./component/Page3/UI/QuizContainer"
+
+function App() {
+  // TODO roll back to Page4s
+  return <Page1></Page1>
+>>>>>>> Stashed changes
 }
 
 export default App;
