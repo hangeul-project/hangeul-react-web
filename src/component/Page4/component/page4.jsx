a331dbab0869669978d2ecb6975ea80c6202ae2f
zinozzing <rlawlsgh1111@naver.com>
2021-08-31 22:31:56 +0900

    Revise Page4_1 global and global things

--- src/component/Page4/component/page4.jsx
+++ src/component/Page4/component/page4.jsx
@@ -1,6 +1,7 @@
-import React from "react"
-import "../Page/Page4_css/page4.css"
-import charA from "../img/img.png"
+import React from "react";
+import "../Page/Page4_css/page4.css";
+import charA from "../img/img.png";
+import textback from "../img/text_back.png";
 
 function Page4() {
   return (
@@ -9,27 +10,33 @@ function Page4() {
         <div className="you">당신은...</div>
         <div className="youris">
           <img className="char" src={charA} alt="캐릭터" />
-          <div className="you__char">
+          <div className="text">
+            <img className="textback" src={textback} alt="textback" />
             <div className="description">
               당신은 한글나라의 <br /> 최고 인기아이돌 김씨소년단입니다!
-              <div className="choice">
-                <button type="button" className="choice__first">
-                  끝내겠소
-                </button>
-                <button type="button" className="choice__second">
-                  기부하겠소
-                </button>
-                <button type="button" className="choice__third">
-                  공유하겠소
-                </button>
-              </div>
+              <choiceBtn />
             </div>
           </div>
         </div>
         <div className="char__name">킹왕짱 세종</div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Page4
+function choiceBtn() {
+  return (
+    <div className="choice">
+      <button type="button" className="choice__first">
+        끝내겠소
+      </button>
+      <button type="button" className="choice__second">
+        기부하겠소
+      </button>
+      <button type="button" className="choice__third">
+        공유하겠소
+      </button>
+    </div>
+  );
+}
+export default Page4;
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,18 +1,18 @@
-import React from "react"
-import "./App.css"
-import PageScroll from "../main/Scroll"
+import React from "react";
+import "./App.css";
+import PageScroll from "../main/Scroll";
 // import QuizContainer from "../component/Page3/UI/QuizContainer";
-import Page3 from "../component/Page3/Page3"
-import Page1 from "../component/Page1/Page1"
-import Page4 from "../component/Page4/component/page4"
+import Page3 from "../component/Page3/Page3";
+import Page1 from "../component/Page1/Page1";
+import Page4 from "../component/Page4/component/page4";
 
 export default function App() {
   return (
     <>
       <div>
         {/* <PageScroll /> */}
-        <Page4></Page4>
+        <Page4 />
       </div>
     </>
-  )
+  );
 }
