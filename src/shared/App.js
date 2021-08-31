839d9fd3b99c9e1f51057c007e6fbf8a0ec1e4ea
zinozzing <rlawlsgh1111@naver.com>
2021-08-31 13:50:51 +0900

    Struct Page4

--- src/shared/App.js
+++ src/shared/App.js
@@ -4,13 +4,14 @@ import PageScroll from "../main/Scroll"
 // import QuizContainer from "../component/Page3/UI/QuizContainer";
 import Page3 from "../component/Page3/Page3"
 import Page1 from "../component/Page1/Page1"
+import Page4 from "../component/Page4/component/page4"
 
 export default function App() {
   return (
     <>
       <div>
         {/* <PageScroll /> */}
-        <Page1></Page1>
+        <Page4></Page4>
       </div>
     </>
   )
