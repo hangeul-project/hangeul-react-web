622cb65503348c65d7342ba92eabf075d9bebe66
zinozzing <rlawlsgh1111@naver.com>
2021-09-01 00:08:15 +0900

    Add button and btn effect

--- src/component/Page4/component/page4.jsx
+++ src/component/Page4/component/page4.jsx
@@ -3,6 +3,20 @@ import "../Page/Page4_css/page4.css";
 import charA from "../img/img.png";
 import textback from "../img/text_back.png";
 
+const ChoiceBtn = () => (
+  <div className="choice">
+    <button type="button" className="choice__first">
+      끝내겠소
+    </button>
+    <button type="button" className="choice__second">
+      기부하겠소
+    </button>
+    <button type="button" className="choice__third">
+      공유하겠소
+    </button>
+  </div>
+);
+
 function Page4() {
   return (
     <div className="outer-container-page4">
@@ -14,7 +28,7 @@ function Page4() {
             <img className="textback" src={textback} alt="textback" />
             <div className="description">
               당신은 한글나라의 <br /> 최고 인기아이돌 김씨소년단입니다!
-              <choiceBtn />
+              <ChoiceBtn />
             </div>
           </div>
         </div>
@@ -24,19 +38,4 @@ function Page4() {
   );
 }
 
-function choiceBtn() {
-  return (
-    <div className="choice">
-      <button type="button" className="choice__first">
-        끝내겠소
-      </button>
-      <button type="button" className="choice__second">
-        기부하겠소
-      </button>
-      <button type="button" className="choice__third">
-        공유하겠소
-      </button>
-    </div>
-  );
-}
 export default Page4;
