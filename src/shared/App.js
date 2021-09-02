f1820807270fd80cdbbf9b3107060877b1904cb9
zinozzing <rlawlsgh1111@naver.com>
2021-09-02 14:46:56 +0900

    Revise 7

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import PageScroll from "../main/Scroll";
-import Page4 from "../../src/component/Page4/component/Page4";
+import Page4 from "../component/Page4/component/page4";
 // import Page3 from "../component/Page3/Page3"; TODO: Uncomment this for checking only Page3
 
 // Rollback changes in this file before push or pull request !
@@ -12,7 +12,7 @@ export default function App() {
         {/* <PageScroll /> */}
         {/* <Page3 /> */}
         {/* TODO: Uncomment this for checking only Page3 */}
-        <Page4 />
+        <Page4></Page4>
       </div>
     </>
   );
