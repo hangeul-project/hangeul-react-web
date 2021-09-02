aa20cbd9bb2aef49ee7b06e30e3baf6460c4d482
hanyangaiis@subwaymate.cafe24app.com:hanyangaiis_subwaymate <rlawlsgh1111@naver.com>
2021-09-02 09:42:49 +0900

    firsr commit #40

--- src/component/Page4/component/page4s.jsx
+++ src/component/Page4/component/page4s.jsx
@@ -1,37 +1,11 @@
 import React from "react";
 import "../Page/Page4_css/page4s.css";
-import charA from "./img.png";
+import background from "../img/background.png";
 
 function page4s() {
   return (
     <>
-      <div className="container">
-        <div className="youris">당신은...</div>
-        <div className="content__left">
-          <div className="content">
-            <img
-              className="char"
-              src={charA}
-              width="400"
-              height="auto"
-              alt="캐릭터"
-            />
-            <div className="name">한글나라 아이돌 김씨소년단</div>
-          </div>
-          <div className="content__right">
-            <div className="where">어디로 공유하시겠소?</div>
-            <div className="btn__social">
-              <button type="button">카카오톡</button>
-              <button type="button">페이스북</button>
-              <button type="button">트위터</button>
-              <button type="button">인스타그램</button>
-            </div>
-            <div className="btn">
-              <button type="button">끝내겠소</button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <div className="background"></div>
     </>
   );
 }
