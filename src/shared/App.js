03326705cdc77d5eae63620bfe5171be43adf591
zinozzing <rlawlsgh1111@naver.com>
2021-09-02 14:40:50 +0900

    Revise 5

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import PageScroll from "../main/Scroll";
-import Page4 from "../../src/component/Page4/component/Page4";
+import Page4 from "../component/Page4/component/Page4";
 // import Page3 from "../component/Page3/Page3"; TODO: Uncomment this for checking only Page3
 
 // Rollback changes in this file before push or pull request !
