d447f728b6ea65e73bac23943268028e6e6b05a6
junho7359 <chlwnsgh1004@naver.com>
2021-09-07 10:19:42 +0900

    feat/#25_revise_page2

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 import PageScroll from "../main/Scroll";
+// import page3 from "../component/page3/page3"
+// import page4 from "../component/page4/page4"
 
 // Rollback changes in this file before push or pull request !
 export default function App() {
@@ -8,6 +10,8 @@ export default function App() {
     <>
       <div>
         <PageScroll />
+        {/* page4 확인용 */}
+        {/* <page4 />} */}
       </div>
     </>
   );
