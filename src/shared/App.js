a3e65e8d64ea20fb3a857ba57d7f6d5f60ad16fc
junho7359 <chlwnsgh1004@naver.com>
2021-09-10 15:14:24 +0900

    feat/#53_mediaquery

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "./App.css";
 import PageScroll from "../main/Scroll";
-// import page3 from "../component/page3/page3"
-// import page4 from "../component/page4/page4"
+import Page4 from "../component/Page4/component/page4";
+// import Page3 from "../component/Page3/Page3"; TODO: Uncomment this for checking only Page3
 
 // Rollback changes in this file before push or pull request !
 export default function App() {
   return (
     <>
       <div>
-        <PageScroll />
-        {/* page4 확인용 */}
-        {/* <page4 />} */}
+        {/* <PageScroll /> */}
+        {/* <Page3 /> */}
+        {/* TODO: Uncomment this for checking only Page3 */}
+        <Page4 />
       </div>
     </>
   );
