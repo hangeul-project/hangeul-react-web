c8aea1da53856959234b593a2a852f7312c6f33d
zinozzing <rlawlsgh1111@naver.com>
2021-10-02 15:15:07 +0900

    arrange-ing P3-2

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import "./App.css";
-// import Page1 from "../component/Page1/Page1";
-// import Page2 from "../component/Page2/Page2";
+import Page1 from "../component/Page1/Page1";
+import Page2 from "../component/Page2/Page2";
 import Page3 from "../component/Page3/Page3";
 // import Page4 from "../component/Page4/component/Page4";
 // import Page4Sharing from "../component/Page4/component/Page4Sharing";
-// import Page5 from "../component/Page5/Page5";
+import Page5 from "../component/Page5/Page5";
 
 // Rollback changes in this file before push or pull request !
 export default function App() {
@@ -18,24 +18,24 @@ export default function App() {
   }, []);
   return (
     <>
-      {/* <div>
+      <div>
         <Page1 />
       </div>
       <div>
         <Page2 />
-      </div> */}
+      </div>
       <div>
         <Page3 />
       </div>
       {/* <div>
         <Page4 />
-      </div>
-      <div>
+      </div> */}
+      {/* <div>
         <Page4Sharing />
-      </div>
+      </div> */}
       <div>
         <Page5 />
-      </div> */}
+      </div>
     </>
   );
 }
